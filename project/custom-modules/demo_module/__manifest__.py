6fd71a09498e1d440a58ab61485cca0170543bc3
akshaybhadange <a.bhadange@kokonetworks.com>
2022-08-28 01:29:14 +0530

    First Commit

---
+++ project/custom-modules/demo_module/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of AktivSoftware See LICENSE file for full copyright
+# and licensing details.
+{
+    'name': "Demo Odoo Module",
+
+    'summary': """
+        Demo""",
+
+    'description': """
+        Demo""",
+
+    'author': "Akshay Bhadange",
+    'website': "http://www.demo.com",
+    'category': 'web',
+    'version': '11.0.1.0.0',
+    'license': "AGPL-3",
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+
+    ],
+    'installable': True,
+    'application': True
+}
